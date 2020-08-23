f952aceedebf766cf723c6090a6c32fe6c71f7a7
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-23 23:29:58 +0300

    close application after work is done

--- src/main/java/com/demo/weather/WeatherApplication.java
+++ src/main/java/com/demo/weather/WeatherApplication.java
@@ -9,8 +9,10 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +26,8 @@ public class WeatherApplication implements CommandLineRunner {
     private WeatherApiClient weatherApiClient;
     @Autowired
     private CsvSuggestionConverter csvSuggestionConverter;
+    @Autowired
+    private ConfigurableApplicationContext context;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -41,7 +45,7 @@ public class WeatherApplication implements CommandLineRunner {
         for(String city : args) {
             writeCityWeather(city.trim());
         }
-        return;
+        context.close();
     }
 
     public void writeCityWeather(String cityName) {
